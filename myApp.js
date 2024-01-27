6b736410edc7f4cdeb8d6ef0c610dce02d76cb60
tejasvijb <jbtejasvi@gmail.com>
2024-01-27 22:52:38 +0530

    add bodyparser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 require('dotenv').config()
 let app = express();
+const bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 
