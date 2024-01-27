0a88197d83adfc6b10dce820377d382e2ccb385e
tejasvijb <jbtejasvi@gmail.com>
2024-01-27 22:44:07 +0530

    add /name

--- myApp.js
+++ myApp.js
@@ -37,6 +37,11 @@ app.get('/:word/echo', (req, res) => {
     res.json({ echo: req.params.word })
 })
 
+app.route('/name').get((req, res) => {
+    const { first, last } = req.query;
+    res.json({ name: `${first} ${last}` })
+})
+
 
 
 
